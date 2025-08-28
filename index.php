012afbf12a08270a136fc3608d4e9900c95e1cf2
Abdullah Essam <abdoessam.2010@gmail.com>
2025-08-28 03:03:10 +0300

    first version

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+
+$accessToken = getenv('FB_ACCESS_TOKEN'); // نخلي التوكن متخزن في Secrets
+$pageId = getenv("FB_PAGE_ID");
+
+// مصفوفة أحاديث
+include "ahadith.php";
+
+$hadith = $ahadith[array_rand($ahadith)];
+
+$url = "https://graph.facebook.com/{$pageId}/feed";
+
+$data = [
+    'message' => $hadith,
+    'access_token' => $accessToken
+];
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+$response = curl_exec($ch);
+curl_close($ch);
+
+echo $response;
